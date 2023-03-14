879c56257e5d0bb27a681286b9fcd3008c04addb
JohnEdwZ <edward.rodriguez.v@live.com.mx>
2023-03-13 22:33:51 -0500

    java

---
+++ src/main/java/com/mycompany/escuela/Modelo/Conexion.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.escuela.Modelo;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author edwar
+ */
+public class Conexion {
+
+    public static Connection getConexion() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
